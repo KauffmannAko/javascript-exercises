a76704eba5b61e9ed18d72bfb6b7b955d74afabe
KauffmannAko <nanakauffmann@gmqil.com>
2022-09-25 19:21:37 +0100

    solve exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(str,n) {
+let my_str = []
+
+if( n < 0)
+{
+    my_str.push("ERROR")
+}
+else
+{
+    const number = Math.floor(Math.random() * 1000)
+    for(let i = 0; i < n; i++)
+    {
+        my_str.push(str);
+    }
+}
+return my_str.join('');
 
 };
+//console.log(repeatString('hey',0));
 
 // Do not edit below this line
 module.exports = repeatString;
